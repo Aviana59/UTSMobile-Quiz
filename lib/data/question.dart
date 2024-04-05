fd67c14d96f3c9fa5685187954e6904c26c63f45
22091397059 Aviana Candra <aviana.22059@mhs.unesa.ac.id>
2024-04-05 20:14:38 +0700

    upload polos

---
+++ lib/data/question.dart
@@ -0,0 +1,104 @@
+import 'package:quiz/models/quiz_question.dart';
+
+//kelas question untuk list soal dan jawaban
+const questions = [
+  QuizQuestion(
+    '1. Fungsi dari Authentication server adalah...',
+    [
+      'Untuk mengenali user yang berintegrasi ke jaringan dan memuat semua informasi dariuser tersebut.',
+      'Untuk melihat pengguna yang masuk ',
+      'Untuk membatasi akun yang masuk ',
+      'Jawaban B dan C benar ',
+      'Semua jawaban salah',
+    ],
+  ),
+  QuizQuestion(
+      '2. Symantec Norton Personal Firewall, Keri0 Personal adalah contoh dari...',
+      [
+        'Personal Firewall',
+        'Firewall',
+        'Packet Filter Firewall',
+        'Softpedia',
+        'Blayteam',
+      ]),
+  QuizQuestion(
+    '3. Berikut ini merupakan fungsi dari Firewall, kecuali...',
+    [
+      'Melakukan pembuatan virus',
+      'Mengatur dan mengontrol lalu lintas jaringan',
+      'Mencatat semua kejadian, dan melaporkan kepada administrator ',
+      'Melakukan autentikasi terhadap akses',
+      'Melindungi sumber daya dalam jaringan private',
+    ],
+  ),
+  QuizQuestion(
+    '4.	Proses yang dilakukan oleh firewall untuk ‘menghadang’dan memproses data dalam sebuah paket untuk menentukan bahwa paket tersebut diizinkan atau ditolak, berdasarkan kebijakanakses (access policy) yang diterapkan oleh seorang administrator adalah...',
+    [
+      'Inspeksi paket',
+      'Drop paket',
+      'Access paket',
+      'Firewall',
+      'Anti-spam',
+    ],
+  ),
+  QuizQuestion(
+    '5. Berikut ini adalah jenis jenis firewall, kecuali...',
+    [
+      'Multilayer Inspection Firewall',
+      'Packet Filtering Gateway',
+      'Circuit Level Gateway',
+      'Statefull Multilayer Inspection Firewall',
+      'Application Layer Gateway',
+    ],
+  ),
+  QuizQuestion(
+    '6.	Jenis firewall yang memberikan fitur terbanyak dan memeberikan tingkat keamanan yang paling tinggi adalah...',
+    [
+      'Statefull Multilayer Inspection Firewall',
+      'Packet Filtering Gateway',
+      'Application Layer Gateway',
+      'Circuit Level Gateway',
+      'Multilayer Inspection Firewall',
+    ],
+  ),
+  QuizQuestion(
+    '7.	Yang termasuk jenis metode enkripsi adalah...',
+    [
+      'SSL',
+      'SDSL',
+      'SASL',
+      'SHS',
+      'RHR',
+    ],
+  ),
+  QuizQuestion(
+    '8.	Sekumpulan teknik untuk memanipulasi orang sehingga orang tersebut membocorkan informasi rahasia disebut...',
+    [
+      'Social Engineering',
+      'Account compromise',
+      'Packet Snifer',
+      'Denial-of-Serviced.Probe',
+      'Probe',
+    ],
+  ),
+  QuizQuestion(
+    '9.	Mensyaratkan bahwa pengirim suatu informasi dapat diidentifikasi dengan benar dan adajaminan bahwa identitas yang didapat tidak palsu adalah...',
+    [
+      'Authentication',
+      'Integrity',
+      'Confidentiality',
+      'Nonrepudiation',
+      'Availability',
+    ],
+  ),
+  QuizQuestion(
+    '10.Suatu proses mengamankan suatu informasi dengan membuat informasi tersebut tidak dapat dibaca tanpa bantuan pengetahuan khusus, disebut sebagai',
+    [
+      'Enkripsi',
+      'Dekripsi',
+      'Enkidu',
+      'Residu',
+      'Integrity',
+    ],
+  ),
+];
